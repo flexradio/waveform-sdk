306971d48252d226e2cb16949cceedf0bc31f3e2
Annaliese McDermond <anna@flex-radio.com>
2020-06-01 08:09:00 -0700

    Change license to LGPL
    If the code was GPL, it wouldn't be able to be linked with the user's
    application without making the whole application public under the GPL.
    This would be largely incompatible with what our government/commerical
    customers are trying to do.  Change this to the LGPL so that they only
    have to re-contribute their internal modifications to the Waveform API
    itself.

--- include/waveform.h
+++ include/waveform.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-3.0-or-later
 /// @file waveform.h
 /// @brief Public definitions of waveform library functions
 /// @authors Annaliese McDermond <anna@flex-radio.com>
@@ -6,15 +6,15 @@
 /// @copyright Copyright (c) 2020 FlexRadio Systems
 ///
 /// This program is free software: you can redistribute it and/or modify
-/// it under the terms of the GNU General Public License as published by
+/// it under the terms of the GNU Lesser General Public License as published by
 /// the Free Software Foundation, version 3.
 ///
 /// This program is distributed in the hope that it will be useful, but
 /// WITHOUT ANY WARRANTY; without even the implied warranty of
 /// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-/// General Public License for more details.
+/// Lesser General Public License for more details.
 ///
-/// You should have received a copy of the GNU General Public License
+/// You should have received a copy of the GNU Lesser General Public License
 /// along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///
 
