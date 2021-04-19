3963c242353753d64ac6d5c97407cb776c4ca4e0
Annaliese McDermond <anna@flex-radio.com>
2021-04-19 07:35:33 -0700

    Split out development package and implement multiarch
    Split out the package into a main and development so that
    we don't have to install extra stuff in production.  Also
    split the development package into proper multiarch so that
    both arches can be installed at the same time.

--- cmake/Modules/FindLibEvent.cmake
+++ cmake/Modules/FindLibEvent.cmake
@@ -1,23 +1,17 @@
-find_path(LibEvent_INCLUDE_DIRS NAMES event.h)
 find_library(LibEvent_LIBRARY NAMES event)
-find_library(LibEvent_CORE NAMES event_core)
-find_library(LibEvent_EXTRA NAMES event_extra)
 find_library(LibEvent_THREAD NAMES event_pthreads)
 
 include(FindPackageHandleStandardArgs)
 
 set(LibEvent_LIBRARIES
         ${LibEvent_LIBRARY}
-        ${LibEvent_CORE}
-        ${LibEvent_EXTRA}
         ${LibEvent_THREAD}
-        ${LibEvent_EXTRA})
+)
 
-find_package_handle_standard_args(LibEvent DEFAULT_MSG LibEvent_LIBRARIES LibEvent_INCLUDE_DIRS)
+find_package_handle_standard_args(LibEvent DEFAULT_MSG LibEvent_LIBRARIES)
 
 if (LibEvent_FOUND AND NOT TARGET LibEvent::LibEvent)
     add_library(LibEvent::LibEvent INTERFACE IMPORTED)
     set_target_properties(LibEvent::LibEvent PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${LibEvent_INCLUDE_DIRS}"
             INTERFACE_LINK_LIBRARIES "${LibEvent_LIBRARIES}")
 endif ()
